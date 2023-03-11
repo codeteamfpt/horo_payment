bc1e7522e7632470c8bc6e83442b5c24557aa403
namlh41 <namlhhe140778@fpt.edu.vn>
2023-03-11 17:13:32 +0700

    build new

---
+++ src/main/java/fpt/horo/payment/dto/request/gateway_request/GetResultRequest.java
@@ -0,0 +1,33 @@
+package fpt.horo.payment.dto.request.gateway_request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GetResultRequest {
+
+    @JsonProperty("billcode")
+    String billcode;
+    @JsonProperty("cust_msisdn")
+    String custMsisdn;
+    @JsonProperty("error_code")
+    String error_code;
+    @JsonProperty("merchant_code")
+    String merchantCode;
+    @JsonProperty("order_id")
+    Integer orderId;
+    @JsonProperty("payment_status")
+    Long paymentStatus;
+    @JsonProperty("trans_amount")
+    Double transAmount;
+    @JsonProperty("vt_transaction_id")
+    String vtTransactionId;
+    @JsonProperty("check_sum")
+    String checkSum;
+}
---
+++ src/main/java/fpt/horo/payment/dto/response/gateway_response/GetResultResponse.java
@@ -0,0 +1,28 @@
+package fpt.horo.payment.dto.response.gateway_response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GetResultResponse {
+    @JsonProperty("error_code")
+    String errorCode;
+    @JsonProperty("merchant_code")
+    String merchantCode;
+    @JsonProperty("order_id")
+    String orderId;
+    @JsonProperty("return_url")
+    String returnUrl;
+    @JsonProperty("return_bill_code")
+    String returnBillCode;
+    @JsonProperty("return_other_in")
+    String returnOtherIn;
+    @JsonProperty("check_sum")
+    String checkSum;
+}
